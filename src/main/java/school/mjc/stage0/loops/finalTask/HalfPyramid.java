e79f83c9186e967d2b0f7e3027e90f3b88b9e693
AliAmet <ametov1ali@gmail.com>
2023-11-11 16:02:51 +0600

    first commit

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for(int i=0; i<cathetusLength; i++)
+        {
 
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for(int j=2*(cathetusLength-i); j>=0; j--)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for(int j=0; j<=i; j++)
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i=0; i<cathetusLength; i++)
+        {
+
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for (int j=cathetusLength-i; j>1; j--)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for (int j=0; j<=i; j++ )
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
 
     }
 
